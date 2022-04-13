dbbb42f6994c788bd38b3d57aab658d11e53e855
Anderson Marlon <23272064+Yagasaki7K@users.noreply.github.com>
2022-04-13 16:01:36 -0300

    Change component to element

--- src/main.jsx
+++ src/main.jsx
@@ -10,20 +10,22 @@ import Carreiras from './pages/Carreiras'
 import ComoFunciona from './pages/ComoFunciona'
 import SignUp from './pages/SignUp'
 
-import { BrowserRouter as Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 ReactDOM.render(
     <React.StrictMode>
 
-        <Switch>
-            <Route path="/" component={App} exact />
-            <Route path="/pets" component={Pets} exact />
-            <Route path="/sobre-nos" component={SobreNos} exact />
-            <Route path="/fale-conosco" component={FaleConosco} exact />
-            <Route path="/carreiras" component={Carreiras} exact />
-            <Route path="/como-funciona" component={ComoFunciona} exact />
-            <Route path="/signup" component={SignUp} exact />
-        </Switch>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App/>} />
+                <Route path="/pets" element={<Pets/>} exact />
+                <Route path="/sobre-nos" element={<SobreNos/>} exact />
+                <Route path="/fale-conosco" element={<FaleConosco/>} exact />
+                <Route path="/carreiras" element={<Carreiras/>} exact />
+                <Route path="/como-funciona" element={<ComoFunciona/>} exact />
+                <Route path="/signup" element={<SignUp/>} exact />
+            </Routes>
+        </BrowserRouter>
 
     </React.StrictMode>,
     document.getElementById('root')
