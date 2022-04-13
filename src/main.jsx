255d3fa7e7b2153da3ab696c741a0b6d2d918486
Anderson Marlon <23272064+Yagasaki7K@users.noreply.github.com>
2022-04-13 15:54:24 -0300

    Update react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -10,23 +10,21 @@ import Carreiras from './pages/Carreiras'
 import ComoFunciona from './pages/ComoFunciona'
 import SignUp from './pages/SignUp'
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Switch, Route } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
+    <React.StrictMode>
 
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/pets" element={<Pets />} />
-        <Route path="/sobre-nos" element={<SobreNos />} />
-        <Route path="/fale-conosco" element={<FaleConosco />} />
-        <Route path="/carreiras" element={<Carreiras />} />
-        <Route path="/como-funciona" element={<ComoFunciona />} />
-        <Route path="/signup" element={<SignUp />} />
-      </Routes>
-    </Router>
+        <Switch>
+            <Route path="/" component={App} exact />
+            <Route path="/pets" component={Pets} exact />
+            <Route path="/sobre-nos" component={SobreNos} exact />
+            <Route path="/fale-conosco" component={FaleConosco} exact />
+            <Route path="/carreiras" component={Carreiras} exact />
+            <Route path="/como-funciona" component={ComoFunciona} exact />
+            <Route path="/signup" component={SignUp} exact />
+        </Switch>
 
-  </React.StrictMode>,
-  document.getElementById('root')
+    </React.StrictMode>,
+    document.getElementById('root')
 )
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [reactRefresh()]
 })
