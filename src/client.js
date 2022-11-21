7376d7a831ee5e40685643581dc923e401b5c73d
Anderson Marlon <anderson18.marlon@gmail.com>
2022-11-21 12:27:28 -0300

    Update modules + EditorConfig

--- src/client.js
+++ src/client.js
@@ -3,13 +3,13 @@ import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBULLnC_uOOs-ayEoisl9rmfnbuOPmv9pg",
-  authDomain: "find-your-pet-945c4.firebaseapp.com",
-  databaseURL: "https://find-your-pet-945c4-default-rtdb.firebaseio.com",
-  projectId: "find-your-pet-945c4",
-  storageBucket: "find-your-pet-945c4.appspot.com",
-  messagingSenderId: "539561987299",
-  appId: "1:539561987299:web:3c076c749388d466f049d6"
+  apiKey: "AIzaSyB305sY0GR-z1lnJx2lqnG0kkAr-JR-IeQ",
+  authDomain: "kalify-findyourpet.firebaseapp.com",
+  databaseURL: "https://kalify-findyourpet-default-rtdb.firebaseio.com",
+  projectId: "kalify-findyourpet",
+  storageBucket: "kalify-findyourpet.appspot.com",
+  messagingSenderId: "843679913436",
+  appId: "1:843679913436:web:55e00dc38e1731adbb6b19"
 };
 
 const app = initializeApp(firebaseConfig);
