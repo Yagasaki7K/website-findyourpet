fdd19467434989a53a6dca037cadcda4eebab708
Anderson Marlon <anderson18.marlon@gmail.com>
2023-04-17 22:12:28 -0300

    [Updates] Colors on Utils + Hidden Maps

--- src/components/AddPetButton.jsx
+++ src/components/AddPetButton.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect } from 'react'
+// import React, { useEffect } from 'react'
+import React from 'react'
 import PetButtonDetails from './AddPetButtonDetails'
 
 function RedirectToSignUp() {
