b81694cba0ac2f59dd8b84ff70178d928a7fb823
Anderson Marlon <anderson18.marlon@gmail.com>
2023-06-21 12:41:29 -0300

    [Firebase] Fixed database development to production

--- src/services/pet.services.js
+++ src/services/pet.services.js
@@ -1,32 +1,38 @@
-import database from '../client'
-import { collection, getDocs, getDoc, addDoc, updateDoc, deleteDoc, doc } from 'firebase/firestore'
+import database from "../client";
+import {
+    collection,
+    getDocs,
+    getDoc,
+    addDoc,
+    deleteDoc,
+    doc,
+} from "firebase/firestore";
 
-const petCollectionRef = collection(database, 'pets-dev')
+const petCollectionRef = collection(database, "pets");
 
 class PetService {
     addPets = (newPets) => {
-        return addDoc(petCollectionRef, newPets)
-    }
+        return addDoc(petCollectionRef, newPets);
+    };
 
     updatePets = (id, updateDoc) => {
-        const petDoc = doc(database, 'pets', id)
-        return updateDoc(petDoc, updateDoc)
-    }
+        const petDoc = doc(database, "pets", id);
+        return updateDoc(petDoc, updateDoc);
+    };
 
     deletePets = (id) => {
-        const petDoc = doc(database, 'pets', id)
-        return deleteDoc(petDoc)
-    }
+        const petDoc = doc(database, "pets", id);
+        return deleteDoc(petDoc);
+    };
 
     getAllPets = () => {
-        return getDocs(petCollectionRef)
-    }
+        return getDocs(petCollectionRef);
+    };
 
     getPet = (id) => {
-        const petDoc = doc(database, 'pets', id)
-        return getDoc(petDoc)
-    }
+        const petDoc = doc(database, "pets", id);
+        return getDoc(petDoc);
+    };
 }
 
-
-export default new PetService()
+export default new PetService();
