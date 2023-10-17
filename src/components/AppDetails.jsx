1e3e19ca4f35a05e02916a8fc6f7a5f68b80c8cf
Anderson Marlon <anderson18.marlon@gmail.com>
2023-10-17 17:33:05 -0300

    Update Info by Windows

--- src/components/AppDetails.jsx
+++ src/components/AppDetails.jsx
@@ -3,294 +3,399 @@ import Colors from '../utils/Colors'
 
 const HomeDetails = styled.div`
 
-    color: ${(props) => (props.theme.modo.darkTheme === 'darkTheme' ? Colors.lightColor : Colors.lightColor)}; 
+color: ${(props) => (props.theme.modo.darkTheme === 'darkTheme' ? Colors.lightColor : Colors.lightColor)};
 
-    position: relative;
-    
-    a {
-        font-weight: 400;
-    }
+position: relative;
 
-    *{
-        padding: 0;
-        margin: 0;
-    }
-  /* This code adjusts the position of an image based on the screen size */
-    @media (max-width: 414px) {
+a {
+    font-weight: 400;
+}
+
+* {
+    padding: 0;
+    margin: 0;
+}
+
+@media (max-width: 414px) {
     left: 3%;
+}
+
+@media (max-width: 360px) {
+    left: 20%;
+}
+
+ul {
+    display: flex;
+    width: 100%;
+    padding-top: 1rem;
+    align-items: center;
+    justify-content: center;
+
+    @media (max-width: 1366px) {
+        padding-top: 0.6rem;
     }
 
-    @media (max-width: 360px) {
-     left: 20%;
-    }
+    img {
+        width: 20rem;
+        margin-left: 4rem;
+        margin-right: 4rem;
 
+        @media (max-width: 1920px) {
+            margin-top: 2rem;
+        }
 
-
-
-    ul {
-        display: flex;
-        width: 100%;
-        padding-top: 1rem;
-        align-items: center;
-        justify-content: center;
-
+        @media (max-width: 1600px) {
+            margin-left: -2rem;
+            margin-top: 1rem;
+        }
 
         @media (max-width: 1366px) {
-            padding-top: 0.6rem;
+            margin-top: 0.6rem;
         }
+
+        @media (max-width: 1440px) {
+            margin-left: 3rem;
+        }
+
+        @media (max-width: 1366px) {
+            margin-left: 1.5rem;
+        }
+
+        @media (max-width: 1024px) {
+            margin-top: 5rem;
+        }
+
+        @media (max-width: 768px) {
+            margin-left: 10rem;
+        }
+
+        @media (max-width: 600px) {
+            margin-top: 0rem;
+            margin-left: 0rem;
+        }
+    }
+
+    li {
+        margin-right: 2rem;
+        margin-top: 2rem;
+        list-style: none;
+
+        @media (max-width: 1600px) {
+            margin-top: 1rem;
+        }
+
+        @media (max-width: 1366px) {
+            margin-top: 1rem;
+        }
+
+        @media (max-width: 1024px) {
+            display: none;
+        }
+
+        a {
+            color: ${(props) => (props.theme.modo.darkTheme === 'darkTheme' ? Colors.lightColor : Colors.lightColor)};
+            text-decoration: none;
+
+            :hover {
+                transition: 1s;
+                background: ${Colors.primary};
+                background: linear-gradient(90deg, ${Colors.primary} 0%, ${Colors.primaryAlt} 100%);
+                background-clip: text;
+                -webkit-background-clip: text;
+                -webkit-text-fill-color: transparent;
+            }
+        }
+
+    }
+
+    button {
+        background: ${Colors.primary};
+        background: linear-gradient(90deg, ${Colors.primary} 0%, ${Colors.primaryAlt} 100%);
+        color: ${(props) => (props.theme.modo.darkTheme === 'darkTheme' ? Colors.lightColor : Colors.lightColor)};
+
+        text-transform: uppercase;
+        font-family: 'Poppins', sans-serif;
+        font-size: 1.2rem;
+        border-radius: 15px;
+        margin-top: 1.5rem;
+        font-weight: bold;
+
+        @media (max-width: 1920px){
+            margin-left: 45rem;
+        }
+
+        @media (max-width: 1600px) {
+            margin-left: 25rem;
+        }
+
+        @media (max-width: 1440px) {
+            margin-left: 15rem;
+        }
+
+        @media (max-width: 1366px) {
+            margin-left: 10rem;
+        }
+
+        @media (max-width: 1024px) {
+            display: none;
+        }
+
+        height: 2.5rem;
+        width: 10rem;
+
+        cursor: pointer;
+
+        :hover {
+            transition: 2s;
+            background: ${Colors.primaryAlt};
+            background: linear-gradient(90deg, ${Colors.primaryAlt} 0%, ${Colors.primary} 100%);
+        }
+    }
+}
+
+.container {
+    width: 100%;
+
+    .content {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        margin-top: 4rem;
+        font-family: ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,"Apple Color Emoji","Segoe UI Emoji",Segoe UI Symbol,"Noto Color Emoji";
 
         img {
-            width: 20rem;
-            margin-left: 4rem;
-            margin-right: 4rem;
-
-
-            @media (max-width: 1920px) {
-                margin-top: 2rem;
-            }
-
-            @media (max-width: 1600px) {
-              margin-left: -2rem;
-              margin-top: 1rem;
-            }
-
-            @media (max-width: 1366px) {
-                margin-top: 0.6rem;
-            }
-
-            @media (max-width: 1440px) {
-                margin-left: 3rem;
-            }
-
-            @media (max-width: 1366px) {
-                margin-left: 1.5rem;
-            }
-
-            @media (max-width: 1024px) {
-                margin-top: 5rem;
-            }
-
-            @media (max-width: 768px) {
-                margin-left: 10rem;
-            }
-
-            @media (max-width: 600px) {
-                margin-top: 0rem;
-                margin-left: 0rem;
-            }
+            border-radius: 15px;
+            width: 35rem;
         }
 
-        li {
-            margin-right: 2rem;
-            margin-top: 2rem;
-            list-style: none;
+        h1 {
+            font-size: 3.75rem;
+            line-height: 1;
+            font-weight: 900;
+        }
 
-            @media (max-width: 1600px) {
+        p {
+            font-size: 1.25rem;
+            line-height: 1.75rem;
+            color: #b4acac;
+            margin-top: 1rem;
+        }
+
+        .leftSide {
+            margin-right: 5rem;
+            max-width: 40rem;
+
+            button {
+                background: #30ad80;
+                color: #fff;
+                width: 8rem;
+                height: 3rem;
                 margin-top: 1rem;
+                text-transform: uppercase;
+                font-weight: 900;
             }
 
-            @media (max-width: 1366px) {
-                margin-top: 1rem;
-            }
+            .newsletter {
+                input {
+                    width: 25rem;
+                    border-radius: 0.375rem;
+                    border: none;
+                    padding: 0.75rem 1rem;
+                    font-size: 1rem;
+                    line-height: 1.5rem;
+                    margin-top: 1.2rem;
+                }
 
-            @media (max-width: 1024px) {
-                display: none;
-            }
-
-            a {
-                color: ${(props) => (props.theme.modo.darkTheme === 'darkTheme' ? Colors.lightColor : Colors.lightColor)}; 
-      
-                text-decoration: none;
-
-                :hover {
-                    transition: 1s;
-                    background: ${Colors.primary};
-                    background: linear-gradient(90deg, ${Colors.primary} 0%, ${Colors.primaryAlt} 100%);
-                    background-clip: text;
-                    -webkit-background-clip: text;
-                    -webkit-text-fill-color: transparent;
+                button {
+                    background: #30ad80;
+                    color: #fff;
+                    width: 6rem;
+                    height: 2.8rem;
+                    margin-left: 0.5rem;
                 }
             }
 
+            .contact {
+                display: flex;
+                margin-top: 2rem;
+
+                img {
+                    width: 4rem;
+                    border: 1px solid #30ad80;
+                    border-radius: 50px;
+                }
+
+                .right-contact {
+                    padding-left: 1.5rem;
+
+                    h2 {
+                        font-size: 1.125rem;
+                        line-height: 1.75rem;
+                    }
+
+                    h4 {
+                        font-size: 1rem;
+                        line-height: 1.5rem;
+                        color: #b4acac;
+                    }
+                }
+            }
+
+            svg {
+                margin-left: 12rem;
+                color: gray;
+            }
         }
 
-        button {
-            background: ${Colors.primary};
-            background: linear-gradient(90deg, ${Colors.primary} 0%, ${Colors.primaryAlt} 100%);
-            color: ${(props) => (props.theme.modo.darkTheme === 'darkTheme' ? Colors.lightColor : Colors.lightColor)}; 
+        .rightSide {
+            max-width: 40rem;
 
-            text-transform: uppercase;
-            font-family: 'Poppins', sans-serif;
-            font-size: 1.2rem;
-            border-radius: 15px;
-            margin-top: 1.5rem;
-            font-weight: bold;
-
-            @media (max-width: 1920px){
-                margin-left: 45rem;
+            svg {
+                color: gray;
             }
+        }
 
-            @media (max-width: 1600px) {
-                margin-left: 25rem;
-            }
+        .pet1 {
+            margin-left: -23rem;
+        }
 
-            @media (max-width: 1440px) {
-                margin-left: 15rem;
-            }
-
-            @media (max-width: 1366px) {
-                margin-left: 10rem;
-            }
-
-            @media (max-width: 1024px) {
-                display: none;
-            }
-
-            height: 2.5rem;
-            width: 10rem;
-
-            cursor: pointer;
-
-            :hover {
-                transition: 2s;
-                background: ${Colors.primaryAlt};
-                background: linear-gradient(90deg, ${Colors.primaryAlt} 0%, ${Colors.primary} 100%);
-            }
+        .pet4 {
+            margin-top: -25rem;
         }
     }
 
-    .container {
+    .advice {
         display: flex;
         justify-content: center;
         align-items: center;
         text-align: center;
         flex-direction: column;
-        line-height: 5rem;
-        max-width: 800px;
-        margin: 0 auto;
-
-        @media (max-width: 1920px) {
-            margin-top: 8rem;
-        }
-
-        @media (max-width: 1366px) {
-            margin-top: 3rem;
-        }
-
-        @media (max-width: 768px) {
-            margin-left: 5rem;
-        }
-
-        @media (max-width: 600px) {
-            margin-left: 1.5rem;
-        }
+        background: #268864;
+        padding: 8rem;
+        margin-top: 5rem;
+        margin-bottom: -15rem;
 
         h1 {
-            font-size: 5rem;
-            margin-top: 5rem;
-            margin-bottom: 2rem;
-            text-transform: uppercase;
-
-            @media (max-width: 1366px) {
-                font-size: 5rem;
-            }
-
-            @media (max-width: 1024px) {
-                font-size: 2.0rem;
-            }
-
-            @media (max-width: 1024px) {
-                margin-top: 1rem;
-                margin-top: 3rem;
-            }
-
-
-            @media (max-width: 800px) {
-                margin-top: -2rem;
-            }
-
-
-            @media (max-width: 600px) {
-                margin-top: 2rem;
-                font-size: 2rem;
-            }
+            line-height: 2.5rem;
         }
 
-        .love {
-            background: ${Colors.primary};
-            background: linear-gradient(90deg, ${Colors.primary} 0%, ${Colors.primaryAlt} 100%);
-            background-clip: text;
-            -webkit-background-clip: text;
-            -webkit-text-fill-color: transparent;
-        }
+        .client {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            text-align: center;
+            flex-direction: column;
+            margin-top: 3rem;
 
-        button {
-            background: ${Colors.primary};
-            background: linear-gradient(90deg, ${Colors.primary} 0%, ${Colors.primaryAlt} 100%);
-            color: ${(props) => (props.theme.modo.darkTheme === 'darkTheme' ? Colors.lightColor : Colors.lightColor)}; 
-            text-transform: uppercase;
-            font-family: 'Poppins', sans-serif;
-            font-weight: bold;
-            font-size: 1.2rem;
-            border-radius: 25px;
-        
-            
-            cursor: pointer;
-
-            :hover {
-                transition: 2s;
-                background: ${Colors.primaryAlt};
-                background: linear-gradient(90deg, ${Colors.primaryAlt} 0%, ${Colors.primary} 100%);
-            }
-        }
-
-        .links {
-            margin-top: 16rem;
-            margin-left: 2rem;
-
-            @media (max-width: 1920px) {
-                margin-top: 18rem;
+            img {
+                width: 3rem;
+                border-radius: 250px;
+                border: 1px solid #fff;
             }
 
-            @media (max-width: 1600px) {
-                margin-top: 7rem;
-            }
-
-            @media (max-width: 1366px) {
-                margin-top: 6rem;
-            }
-
-            
-            @media (max-width: 1280px) {
-                margin-top: 9rem;
-            }
-
- 
-            @media (max-width: 1024px) {
-                margin-top: 3rem;
-            }
-
-            @media (max-width: 600px) {
-                margin-top: 10rem;
-            }
-
-   
-            a {
-                color: ${(props) => (props.theme.modo.darkTheme === 'darkTheme' ? Colors.lightColor : Colors.lightColor)}; 
-
-            
-                text-decoration: none;
-
-                :hover {
-                    background: ${Colors.primary};
-                    background: linear-gradient(90deg, ${Colors.primary} 0%, ${Colors.primaryAlt} 100%);
-                    background-clip: text;
-                    -webkit-background-clip: text;
-                    -webkit-text-fill-color: transparent;
-                }
+            h4 {
+                margin-top: 0.5rem;
             }
         }
     }
+
+    .joinUs {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        flex-direction: column;
+        padding: 8rem;
+        margin-top: 10rem;
+        margin-bottom: -22rem;
+
+        .content {
+            flex-direction: column;
+            border: 1px solid #ccc;
+            border-radius: 15px;
+            padding: 5rem;
+            background: rgba(0,0,0,0.5);
+
+            h1 {
+                max-width: 70rem;
+                span {
+                    color: #30ad80;
+                }
+            }
+
+            i {
+                max-width: 50rem;
+                margin-top: 2rem;
+                margin-bottom: 2rem;
+                font-size: 1.125rem;
+                line-height: 1.75rem;
+            }
+
+            button {
+                background: #30ad80;
+                font-size: 1rem;
+                color: #fff;
+                width: 8rem;
+                height: 3rem;
+                margin-left: 0.5rem;
+            }
+        }
+
+
+    }
+
+    .links {
+        margin-top: 16rem;
+        display: flex;
+        justify-content: center;
+        text-align: center;
+        flex-direction: column;
+
+        @media (max-width: 1920px) {
+            margin-top: 18rem;
+        }
+
+        @media (max-width: 1600px) {
+            margin-top: 7rem;
+        }
+
+        @media (max-width: 1366px) {
+            margin-top: 6rem;
+        }
+
+
+        @media (max-width: 1280px) {
+            margin-top: 9rem;
+        }
+
+
+        @media (max-width: 1024px) {
+            margin-top: 3rem;
+        }
+
+        @media (max-width: 600px) {
+            margin-top: 10rem;
+        }
+
+        a {
+            color: ${(props) => (props.theme.modo.darkTheme === 'darkTheme' ? Colors.lightColor : Colors.lightColor)};
+            text-decoration: none;
+
+            :hover {
+                background: ${Colors.primary};
+                background: linear-gradient(90deg, ${Colors.primary} 0%, ${Colors.primaryAlt} 100%);
+                background-clip: text;
+                -webkit-background-clip: text;
+                -webkit-text-fill-color: transparent;
+            }
+        }
+
+        p {
+            margin: 1rem 0;
+        }
+    }
+}
 `
 
 export default HomeDetails
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -36,18 +36,123 @@ const Home = () => {
                     <li><a href="/carreiras">Carreiras</a></li>
                     <li><a href="https://discord.gg/jhSepmE7nN" target="_blank" rel='noreferrer'>Suporte</a></li>
                     <li><a href="https://play.google.com/store/apps/details?id=com.kalifyinc.findyourpet&pli=1">Download</a></li>
-                    <button onClick={() => toHome()}>Acessar</button>
+                    <button onClick={() => toHome()}>Entrar</button>
                 </ul>
 
                 <div className="container">
 
-                    <h1>Ajude um <span className="love">PET</span> <br /> a achar seu lar</h1>
-                    <button onClick={() => toHome()}>Acessar</button>
+                    <div className="content">
+                        <div className="leftSide">
+                            <h1>Encontre seu companheiro de estimação perfeito</h1>
+                            <p>Procurando por um amigo de quatro patas? O FindYourPet ajuda você a encontrar e resgatar animais que precisam de um lar amoroso</p>
 
+                            {/* <div className="newsletter">
+                                <input type="text" placeholder='Insira seu email ...' />
+                                <button>Enviar</button>
+                            </div> */}
+
+                            <button onClick={() => toHome()}>Entrar</button>
+
+                            <div className="contact">
+                                <div className="left-contact">
+                                    <img src="https://storage.googleapis.com/mixo-files/public/img/avatars/male-19.png" alt="" />
+                                </div>
+
+                                <div className="right-contact">
+                                    <h2>
+                                        &quot;Encontrei meu melhor amigo através do FindYourPet. É uma plataforma incrível para amantes de animais de estimação.&quot;
+                                    </h2>
+                                    <h4>Pedro Guilherme</h4>
+                                </div>
+
+
+                            </div>
+                        </div>
+
+                        <div className="rightSide">
+                            <svg width="404" height="392" fill="none" viewBox="0 0 404 392"><defs><pattern id="837c3e70-6c3a-44e6-8854-cc48c737b659" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse"><rect x="0" y="0" width="4" height="4" fill="currentColor"></rect></pattern></defs><rect width="404" height="392" fill="url(#837c3e70-6c3a-44e6-8854-cc48c737b659)"></rect></svg>
+
+                            <img src="/assets/pet1.jpg" className="pet1" alt="" />
+                        </div>
+                    </div>
+
+                    <div className="content">
+                        <div className="leftSide">
+                            <img src="/assets/pet2.jpg" className="pet2" alt="" />
+                        </div>
+
+                        <div className="rightSide">
+                            <h1>
+                                Pesquisa fácil de animais de estimação
+                            </h1>
+                            <p>
+                                FindYourPet facilita a busca de animais de estimação com base em suas preferências. Encontre o companheiro perfeito que corresponda ao seu estilo de vida e necessidades.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="content">
+                        <div className="leftSide">
+                            <h1>
+                                Resgatar e salvar vidas
+                            </h1>
+                            <p>
+                                Ao usar o FindYourPet, você pode resgatar animais de estimação necessitados e dar-lhes uma segunda chance na vida. Faça a diferença e salve uma vida hoje.
+                            </p>
+                        </div>
+
+                        <div className="rightSide">
+                            <img src="/assets/pet3.jpg" className="pet3" alt="" />
+                        </div>
+                    </div>
+
+                    <div className="content">
+                        <div className="leftSide">
+                            <svg width="404" height="392" fill="none" viewBox="0 0 404 392"><defs><pattern id="837c3e70-6c3a-44e6-8854-cc48c737b659" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse"><rect x="0" y="0" width="4" height="4" fill="currentColor"></rect></pattern></defs><rect width="404" height="392" fill="url(#837c3e70-6c3a-44e6-8854-cc48c737b659)"></rect></svg>
+                            <img src="/assets/pet4.jpg" className="pet4" alt="" />
+                        </div>
+
+                        <div className="rightSide">
+                            <h1>
+                                Apoie a adoção de animais de estimação
+                            </h1>
+                            <p>
+                                FindYourPet promove a adoção de animais de estimação e apoia abrigos e organizações de resgate. Junte-se a nós para fornecer lares amorosos para animais de estimação necessitados.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="advice">
+                        <h1>
+                            “FindYourPet me ajudou a resgatar um cachorro que foi abandonado.
+                            Sou grato por esta plataforma e pela oportunidade de dar a ele um lar amoroso.”
+                        </h1>
+
+                        <div className="client">
+                            <img src="https://storage.googleapis.com/mixo-files/public/img/avatars/male-31.png" alt="" />
+                            <h4>Vinicius de Oliveira</h4>
+                        </div>
+                    </div>
+
+                    <div className="joinUs">
+                        <div className="content">
+                            <h1>
+                                Junte-se à nossa comunidade de amantes de animais de estimação e faça a diferença<span>.</span>
+                            </h1>
+
+                            <i>“Graças ao FindYourPet, encontrei meu companheiro de estimação perfeito e também contribuí para a missão de salvar animais. É uma situação ganha-ganha.” - Bruna dos Santos</i>
+
+                            <button onClick={() => toHome()}>Entrar</button>
+                        </div>
+                    </div>
 
                     <div className="links">
-                        <a href="https://kalify.vercel.app/termos">Termos de Uso</a> - &nbsp;
-                        <a href="https://kalify.vercel.app/politicas">Políticas de Privacidade</a>
+                        <div className="url">
+                            <a href="https://kalify.vercel.app/termos">Termos de Uso</a>&nbsp; - &nbsp;
+                            <a href="https://kalify.vercel.app/politicas">Políticas de Privacidade</a>
+                        </div>
+
+                        <p>© 2022 - {new Date().getFullYear()} <a href="/">FindYourPet</a> by <a href="https://kalifyinc.vercel.app/" target="_blank" rel="noreferrer">Kalify Inc.</a> | Todas as imagens dos animais são de propriedade de seus devidos anunciantes</p>
                     </div>
                 </div>
             </AppDetails>
