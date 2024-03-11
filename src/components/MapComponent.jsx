258a035a2eaf2f03bef025102e529896e7158620
Anderson Marlon <anderson18.marlon@gmail.com>
2024-03-11 16:51:45 -0300

    Added: Maps

--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -1,19 +1,25 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import styled from 'styled-components'
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from "react-leaflet";
 
 export default function MapComponent() {
     const position = [-22.85942241928221, -47.0496427552949];
 
     return (
-        <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={position}>
-                <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-            </Marker>
-        </MapContainer>
+        <MapDetails>
+            <MapContainer
+                center={position}
+                zoom={17}
+                scrollWheelZoom={false}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+            </MapContainer>
+        </MapDetails>
     );
 }
+
+const MapDetails = styled.div`
+    width: 100%;
+    height: 100vh;
+`
