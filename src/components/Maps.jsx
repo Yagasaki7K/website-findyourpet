956025602539b93a595298f962dbbe335957be35
Anderson Marlon <anderson18.marlon@gmail.com>
2024-05-17 20:02:52 -0300

    Removed: New Fixed Mark - 17-05 20:02

--- src/components/Maps.jsx
+++ src/components/Maps.jsx
@@ -16,7 +16,7 @@ const Maps = () => {
             zoom: 12
         });
 
-        new mapboxgl.Marker({ color: 'red' })
+        new mapboxgl.Marker({ color: 'purple' })
             .setLngLat([-22.856497135943872, -47.05535307439016])
             .setPopup(new mapboxgl.Popup().setHTML("<h3>Animal 1</h3><p>Descrição do animal 1</p>"))
             .addTo(map);
@@ -24,11 +24,6 @@ const Maps = () => {
         new mapboxgl.Marker({ color: 'blue' })
             .setLngLat([-22.851217688320244, -47.06275597127332])
             .setPopup(new mapboxgl.Popup().setHTML("<h3>Animal 2</h3><p>Descrição do animal 2</p>"))
-            .addTo(map);
-
-        new mapboxgl.Marker({ color: 'purple' })
-            .setLngLat([-22.842596127208118, -47.05443039448861])
-            .setPopup(new mapboxgl.Popup().setHTML("<h3>Animal 3</h3><p>Descrição do animal 3</p>"))
             .addTo(map);
 
         return () => map.remove();
